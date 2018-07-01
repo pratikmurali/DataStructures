15cab3601914535d1295386fafe9f7398ede3236
Pratik Murali <pratik.murali@gmail.com>
2018-07-01 11:47:07 -0700

    Pair Utility class

---
+++ src/com/pratik/datastructures/trees/Pair.java
@@ -0,0 +1,47 @@
+package com.pratik.datastructures.trees;
+
+public class Pair<K, V> {
+	
+    public K k;
+    public V v;
+    
+    
+
+	public Pair(K k, V v) {
+		super();
+		this.k = k;
+		this.v = v;
+	}
+
+
+
+	public K getK() {
+		return k;
+	}
+
+
+
+	public void setK(K k) {
+		this.k = k;
+	}
+
+
+
+	public V getV() {
+		return v;
+	}
+
+
+
+	public void setV(V v) {
+		this.v = v;
+	}
+
+
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
