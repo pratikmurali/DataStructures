a6299bc876db479a9547217f64333742fa001414
Pratik Murali <pratik.murali@gmail.com>
2018-07-07 20:32:11 -0700

    UniquePathsII

--- src/com/pratik/datastructures/dp/UniquePathsII.java
+++ src/com/pratik/datastructures/dp/UniquePathsII.java
@@ -7,14 +7,17 @@ package com.pratik.datastructures.dp;
  */
 public class UniquePathsII {
 
+	/*
+	 * Bottom up solution.
+	 */
 	public static int uniquePathsWithObstacles(int[][] gridWithObstacles) {
 
 		int m = gridWithObstacles.length;
 		int n = gridWithObstacles[0].length;
 
 		int[][] DP = new int[m + 1][n + 1];
-		DP[m-1][n] = 1;
-		
+		DP[m - 1][n] = 1;
+
 		for (int i = m - 1; i >= 0; i--) {
 			for (int j = n - 1; j >= 0; j--) {
 				DP[i][j] = (gridWithObstacles[i][j] == 1) ? 0 : DP[i + 1][j] + DP[i][j + 1];
